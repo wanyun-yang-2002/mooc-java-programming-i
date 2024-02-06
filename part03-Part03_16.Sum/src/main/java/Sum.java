8b5b188b9c009cf1c81cea0de4c1e09c04a5ad1e
yangwanyun <ysrdywywork@163.com>
2024-02-06 10:46:08 +0800

    update part3 16

--- part03-Part03_16.Sum/src/main/java/Sum.java
+++ part03-Part03_16.Sum/src/main/java/Sum.java
@@ -6,5 +6,12 @@ public class Sum {
     public static void main(String[] args) {
         // Try your method here
     }
+    public static int sum(ArrayList<Integer> numbers) {
+        int res = 0;
+        for (int i=0; i<numbers.size(); i++) {
+            res += numbers.get(i);
+        }
+        return res;
+    }
 
 }
