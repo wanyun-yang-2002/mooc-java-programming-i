01c33869821793968e13ef18aa7c17de7ca68c40
yangwanyun <ysrdywywork@163.com>
2024-02-06 10:24:35 +0800

    update part3 15

--- part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
+++ part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -6,5 +6,11 @@ public class PrintInRange {
     public static void main(String[] args) {
         // Try your method here
     }
-    
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int i=0; i<numbers.size(); i++) {
+            if (numbers.get(i) >= lowerLimit && numbers.get(i) <= upperLimit) {
+                System.out.println(numbers.get(i));
+            }
+        }
+    }
 }
