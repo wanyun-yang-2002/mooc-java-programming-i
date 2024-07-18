2b820f14a1382a23c69c06d742871280e7b231e9
yangwanyun <ysrdywywork@163.com>
2024-07-18 20:53:42 +0800

    part3_26

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,17 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String sentence = scanner.nextLine();
+            if (sentence.isEmpty()) {
+                break;
+            }
+            String[] word = sentence.split(" ");
+            for (String i : word) {
+                System.out.println(i);
+            }
+        }
+        
 
     }
 }
