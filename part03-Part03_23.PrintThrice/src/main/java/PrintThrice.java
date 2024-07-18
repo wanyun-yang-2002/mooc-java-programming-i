b48b7dbc14a57ee0a917e86d8799ced18ef0827e
yangwanyun <ysrdywywork@163.com>
2024-07-18 20:16:20 +0800

    part3_23

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class PrintThrice {
 
     public static void main(String[] args) {
+        System.out.print("Give a word:");
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        
+        String word = scanner.nextLine();
+        System.out.print(word + word + word);
 
     }
 }
