e765d9ebe25c09c59fe0be93ca893a075af725fd
yangwanyun <ysrdywywork@163.com>
2024-07-19 11:34:02 +0800

    part3_30

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,6 +5,18 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            String words = scanner.nextLine();
+            if (words.isEmpty()) {
+                break;
+            }
+            String[] parts = words.split(",");
+            if (sum < Integer.valueOf(parts[1])) {
+                sum = Integer.valueOf(parts[1]);
+            }
+        }
+        System.out.println("Age of the oldest: " + sum);
 
 
     }
