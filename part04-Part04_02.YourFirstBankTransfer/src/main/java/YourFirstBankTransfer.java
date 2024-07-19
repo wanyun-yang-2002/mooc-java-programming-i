512a14b1e3395691091bc87f4a9cbd9c3290d2d5
yangwanyun <ysrdywywork@163.com>
2024-07-19 16:02:30 +0800

    part4_2

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account Matthews_account = new Account("Matthews account", 1000);
+        Account myAccount = new Account("My account", 0);
+        Matthews_account.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        System.out.println(Matthews_account);
+        System.out.println(myAccount);
     }
 }
