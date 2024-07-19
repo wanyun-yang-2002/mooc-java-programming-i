b19f66c58c6ccafadc7bfd0932c243784275f194
yangwanyun <ysrdywywork@163.com>
2024-07-19 11:04:37 +0800

    part3_29

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,6 +5,14 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String sentence = scanner.nextLine();
+            if (sentence.equals("")) {
+                break;
+            }
+            String[] words = sentence.split(" ");
+            System.out.println(words[words.length - 1]);
+        }
 
 
     }
