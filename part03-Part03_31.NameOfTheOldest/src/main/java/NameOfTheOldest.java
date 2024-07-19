5f90584a040d1903cb4a572f6476f26916f9eae0
yangwanyun <ysrdywywork@163.com>
2024-07-19 11:37:19 +0800

    part3_31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,6 +5,20 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int old = 0;
+        String username = "";
+        while (true) {
+            String words = scanner.nextLine();
+            if (words.isEmpty()) {
+                break;
+            }
+            String[] parts = words.split(",");
+            if (old < Integer.valueOf(parts[1])) {
+                old = Integer.valueOf(parts[1]);
+                username = parts[0];
+            }
+        }
+        System.out.println("Name of the oldest: " + username );
 
 
     }
