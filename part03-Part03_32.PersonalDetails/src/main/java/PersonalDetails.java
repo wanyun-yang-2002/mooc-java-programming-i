6e29b19a27956a0518b37ba845e15be3f3e21fd2
yangwanyun <ysrdywywork@163.com>
2024-07-19 11:54:31 +0800

    part3_32

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,6 +6,25 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int longname = 0;
+        int cnt = 0;
+        int sum = 0;
+        String username = "";
+        while (true) {
+            String words = scanner.nextLine();
+            if (words.isEmpty()) {
+                break;
+            }
+            String[] parts = words.split(",");
+            cnt += 1;
+            sum = sum + Integer.valueOf(parts[1]);
+            if (longname < parts[0].length()) {
+                longname = parts[0].length();
+                username = parts[0];
+            }
+        }
+        System.out.println("Longest name: " + username );
+        System.out.println("Average of the birth years: " + (double) sum / cnt);
 
 
     }
