802624f13ad730e781ecc5bef770c1a1c48a43fb
yangwanyun <ysrdywywork@163.com>
2024-07-19 11:00:44 +0800

    part3_28

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,13 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        while (true) {
+            String sentence = scanner.nextLine();
+            if (sentence.equals("")) {
+                break;
+            }
+            String[] words = sentence.split(" ");
+            System.out.println(words[0]);
+        }
     }
 }
