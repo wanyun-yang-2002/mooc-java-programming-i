b041c3a1ed9a639c7c913b97b294d7eaa56a03f9
yangwanyun <ysrdywywork@163.com>
2024-07-22 20:30:35 +0800

    p4_15

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,27 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        System.out.println("Enter numbers:");
+        Statistics stat = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+
+        while (true) {
+            int input = scanner.nextInt();
+            if (input == -1) {
+                break;
+            }
+            stat.addNumber(input);
+            if (input % 2 == 0) {
+                even.addNumber(input);
+            } else {
+                odd.addNumber(input);
+            }
+        }
+        System.out.println("Sum: " + stat.sum());
+        System.out.println("Sum of even numbers: " + even.sum());
+        System.out.println("Sum of odd numbers: " + odd.sum());
+        
+        
     }
 }
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,36 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        // initialize the variable numberCount here
+        this.count = 0;
+        this.sum = 0;
+    }
+
+    public void addNumber(int number) {
+        // write code here
+        this.count = this.count + 1;
+        this.sum = this.sum + number;
+    }
+
+    public int getCount() {
+        // write code here
+        return this.count;
+    }
+    
+    public int sum() {
+        // write code here
+        return this.sum;
+    }
+
+    public double average() {
+        // write code here
+        if (this.count == 0) {
+            return 0;
+        }
+        double res = (double) this.sum / this.count;
+        return res;
+    }
 }
