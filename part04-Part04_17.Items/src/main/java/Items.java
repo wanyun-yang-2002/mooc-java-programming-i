00785512d74ca366c1c7b3227cb0207bf61f2614
yangwanyun <ysrdywywork@163.com>
2024-07-23 10:53:48 +0800

    p4_17

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -9,6 +9,16 @@ public class Items {
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            System.out.println("Name: ");
+            String username = scanner.nextLine();
+            if (username.isEmpty()) {
+                break;
+            }
+            items.add(new Item(username));
+        }
+        for (Item item: items) {
+            System.out.println(item.toString());
+        }
     }
 }
