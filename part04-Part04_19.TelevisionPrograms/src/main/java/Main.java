81a0bd3682b077581faa4b45ab72a6c83dda0d8f
yangwanyun <ysrdywywork@163.com>
2024-07-23 11:25:05 +0800

    p4_19

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,23 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.println("Duration: ");
+            int duration = scanner.nextInt();
+            scanner.nextLine();  // 消耗换行符
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        System.out.println("Program's maximum duration? ");
+        int maxi = scanner.nextInt();
+        for (TelevisionProgram item: programs) {
+            if (item.getDuration() <= maxi) {
+                System.out.println(item.toString());
+            }
+        }
     }
 }
