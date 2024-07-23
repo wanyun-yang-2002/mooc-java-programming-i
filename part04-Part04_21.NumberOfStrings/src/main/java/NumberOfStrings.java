94df49e868602c9b577370617df2d8ca2989cfb5
yangwanyun <ysrdywywork@163.com>
2024-07-23 12:08:50 +0800

    p4 21

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,15 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int cnt = 0;
+        while (true) {
+            String in = scanner.nextLine();
+            if (in.equals("end")) {
+                break;
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
 
     }
 }
