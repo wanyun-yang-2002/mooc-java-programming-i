4e47e0050494d52a08e17e5c0c7ea7e6080d86db
yangwanyun <ysrdywywork@163.com>
2024-07-23 17:48:10 +0800

    p4_25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,16 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String fileName = scanner.nextLine();
+        try (Scanner file = new Scanner(Paths.get(fileName))) {
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 }
