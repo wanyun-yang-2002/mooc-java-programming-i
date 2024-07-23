87f7e37fe3fcb1eeb394b3f386123dda4e4a9ee1
yangwanyun <ysrdywywork@163.com>
2024-07-23 16:27:44 +0800

    p4_22

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,13 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String in = scanner.nextLine();
+            if (in.equals("end")) {
+                break;
+            }
+            int res = Integer.valueOf(in);
+            System.out.println(res * res * res);
+        }
     }
 }
