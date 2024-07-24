1fc4692b07d891b334584169213e87a099cf6d72
yangwanyun <ysrdywywork@163.com>
2024-07-24 11:05:27 +0800

    p4 29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,17 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Name of the file:");
+        String fileName = scanner.nextLine();
+        try (Scanner file = new Scanner(Paths.get(fileName))) {
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                String[] word = line.split(",");
+                System.out.println(word[0] + ", age: " + word[1] + " years");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 }
