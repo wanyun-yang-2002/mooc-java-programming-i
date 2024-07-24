95c01d556eb3d6046a6166a5f929fc8b27410197
yangwanyun <ysrdywywork@163.com>
2024-07-24 12:19:21 +0800

    p5 1

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 23548
+ */
+public class Timer {
+    private final ClockHand hundredths;
+    private final ClockHand seconds;
+    
+    public Timer() {
+        this.hundredths = new ClockHand(100);
+        this.seconds = new ClockHand(60);
+    }
+    
+    public void advance() {
+        this.hundredths.advance();
+        if (this.hundredths.value() == 0) {
+            this.seconds.advance();
+        }
+    }
+    
+    public String toString() {
+        return this.seconds + ":" + this.hundredths;
+    }
+}
