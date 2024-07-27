a62c934b7ab30600407d65868c4647d7bfb13168
yangwanyun <ysrdywywork@163.com>
2024-07-27 16:57:00 +0800

    p5 9

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,22 @@
 
 public class HealthStation {
-
+    private int cnt;
+    
+    public HealthStation() {
+        this.cnt = 0;
+    }
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.cnt = this.cnt + 1;
+        return person.getWeight();
     }
-
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return this.cnt;
+    }
 }
