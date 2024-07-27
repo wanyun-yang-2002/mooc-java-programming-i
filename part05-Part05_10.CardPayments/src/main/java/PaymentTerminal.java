aa34684ee1040a512e78ff7384b229c0595a12fd
yangwanyun <ysrdywywork@163.com>
2024-07-27 18:18:19 +0800

    download new exercise

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -9,22 +9,46 @@ public class PaymentTerminal {
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000.0;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment - 2.5 >= 0) {
+            this.money = this.money + 2.50;
+            this.affordableMeals = this.affordableMeals + 1;
+            return payment - 2.5;
+        }
+        return payment;
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment - 4.3 >= 0) {
+            this.money = this.money + 4.30;
+            this.heartyMeals = this.heartyMeals + 1;
+            return payment - 4.3;
+        }
+        return payment;
+    }
+    
+    public boolean eatAffordably(PaymentCard card) {
+        // an affordable meal costs 2.50 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
     }
 
+    public boolean eatHeartily(PaymentCard card) {
+        // a hearty meal costs 4.30 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+    }
 
     @Override
     public String toString() {
