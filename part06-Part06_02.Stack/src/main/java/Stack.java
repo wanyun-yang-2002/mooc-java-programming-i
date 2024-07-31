e3aadf92ba61e081f12c3e608324c0834ed8fba1
yangwanyun <ysrdywywork@163.com>
2024-07-31 19:17:07 +0800

    p6 2

---
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 23548
+ */
+import java.util.ArrayList;
+
+public class Stack {
+    private ArrayList<String> stack;
+    
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+    
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+    
+    public void add(String value) {
+        this.stack.add(value);
+    }
+    
+    public ArrayList<String> values() {
+        return this.stack;
+    }
+    
+    public String take() {
+        String res = this.stack.get(this.stack.size() - 1);
+        this.stack.remove(this.stack.size() - 1);
+        return res;
+    }
+}
