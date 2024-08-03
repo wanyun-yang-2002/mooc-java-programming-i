eb8778b033dac08d2e07f6236f1c31e346523f99
yangwanyun <ysrdywywork@163.com>
2024-08-03 11:23:42 +0800

    p7 1

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -1,21 +1,50 @@
-
 import java.util.Scanner;
 
 public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int first = 0;
+        int second = 0;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
-
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if (command.equals("add")) {
+                if (amount > 0) {
+                    first += amount;
+                    if (first > 100) {
+                        first = 100;
+                    }
+                }
+            } else if (command.equals("move")) {
+                if (amount > 0) {
+                    if (amount > first) {
+                        amount = first; // move only available amount
+                    }
+                    first -= amount;
+                    second += amount;
+                    if (second > 100) {
+                        second = 100;
+                    }
+                }
+            } else if (command.equals("remove")) {
+                if (amount > 0) {
+                    second -= amount;
+                    if (second < 0) {
+                        second = 0;
+                    }
+                }
+            }
         }
     }
-
 }
